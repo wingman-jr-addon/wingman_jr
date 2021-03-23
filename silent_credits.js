28ee6bec077c0488378b11a5d4a5bd44596bd579
wingman-jr-addon <wingman.jr.addon@gmail.com>
2021-03-23 00:19:31 -0500

    Replacing insertAdjacentHtml usage to avoid warning in AMO submission

--- silent_credits.js
+++ silent_credits.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     for(let smi=0; smi<SM_DATA.length; smi++) {
         let d = SM_DATA[smi];
-        document.body.insertAdjacentHTML('beforeend',`<a href="${d.credits}" target="_blank"><img src="${d.file}" /></a>`);
+        let a = document.createElement('a');
+        a.href = d.credits;
+        a.target = '_blank';
+        let img = document.createElement('img');
+        img.src = d.file;
+        a.appendChild(img);
+        document.body.appendChild(a);
     }
 });
