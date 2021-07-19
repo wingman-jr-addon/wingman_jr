0a093e9a8236773c159ff94ba4f460bebebfc5ce
wingman-jr-addon <wingman.jr.addon@gmail.com>
2021-07-19 00:05:03 -0500

    Minimal repro

--- whitelist.js
+++
@@ -1,8 +0,0 @@
-function whtIsWhitelisted(url) {
-    return whtWhitelistStems.some(stem=>url.startsWith(stem));
-}
-
-const whtWhitelistStems = [
-    'https://www.google.com/recaptcha',
-    'https://www.gstatic.com/recaptcha'
-];
